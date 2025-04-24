10c9717a3a4d4009d7a9eecbb80111ac7929c0f6
Alicia McNett <alm36@pct.edu>
2025-04-24 11:05:26 -0400

    NEW UI
    Start screen, HUD, and game over

--- Source/Pangaea/PangaeaGameInstance.cpp
+++ Source/Pangaea/PangaeaGameInstance.cpp
@@ -2,4 +2,23 @@
 
 
 #include "PangaeaGameInstance.h"
+#include "Kismet/GameplayStatics.h"
 
+void UPangaeaGameInstance::StartListenServer()
+{
+	auto world = GEngine->GetCurrentPlayWorld();
+	UGameplayStatics::OpenLevel(world, FName(TEXT("/Game/TopDown/Maps/TopDownMap")), true, "?listen");
+}
+
+void UPangaeaGameInstance::JoinAsClient(FString IPAddress)
+{
+	auto world = GEngine->GetCurrentPlayWorld();
+	UGameplayStatics::OpenLevel(world, *IPAddress, true, "?join");
+}
+
+
+void UPangaeaGameInstance::LeaveGame()
+{
+	auto world = GEngine->GetCurrentPlayWorld();
+	UGameplayStatics::OpenLevel(world, FName(TEXT("/Game/TopDown/Maps/LobbyMap")));
+}
--- Source/Pangaea/PangaeaGameInstance.h
+++ Source/Pangaea/PangaeaGameInstance.h
@@ -14,4 +14,15 @@ class PANGAEA_API UPangaeaGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 	
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Pangaea")
+	void StartListenServer();
+
+	UFUNCTION(BlueprintCallable, Category = "Pangaea")
+	void JoinAsClient(FString IPAddress);
+
+	UFUNCTION(BlueprintCallable, Category = "Pangaea")
+	void LeaveGame();
+
 };
